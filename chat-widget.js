8e7ff3557a9f6fe64d9e2c749cb651467066dbd7
depressed7kk1d <111415286+depressed7kk1d@users.noreply.github.com>
2025-04-27 22:00:17 +1000

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -348,7 +348,7 @@
                 <svg class="message-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path fill="currentColor" d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H5.2L4 17.2V4h16v12z"/>
                 </svg>
-                Send us a message
+                Написать
             </button>
             <p class="response-text">${config.branding.responseTimeText}</p>
         </div>
@@ -364,7 +364,7 @@
             <div class="chat-messages"></div>
             <div class="chat-input">
                 <textarea placeholder="Напишите своё сообщение здесь..." rows="1"></textarea>
-                <button type="submit">Send</button>
+                <button type="submit">Отправить</button>
             </div>
             <div class="chat-footer">
                 <a href="${config.branding.poweredBy.link}" target="_blank">${config.branding.poweredBy.text}</a>
