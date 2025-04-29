f4121d919a3f0c8aef48273fcb09026b74274437
depressed7kk1d <111415286+depressed7kk1d@users.noreply.github.com>
2025-04-29 14:23:55 +1000

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -347,7 +347,7 @@
                 <svg class="message-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path fill="currentColor" d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H5.2L4 17.2V4h16v12z"/>
                 </svg>
-                Написать ИИ консультанту
+                Написать виртуальному ассистенту
             </button>
             <p class="response-text">${config.branding.responseTimeText}</p>
         </div>
@@ -362,7 +362,7 @@
             </div>
             <div class="chat-messages"></div>
             <div class="chat-input">
-                <textarea placeholder="Напишите ваш сообщение" rows="1"></textarea>
+                <textarea placeholder="Напишите ваше сообщение" rows="1"></textarea>
                 <button type="submit">Отправить</button>
             </div>
             <div class="chat-footer">
